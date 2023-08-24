bea288ae920753a3bdc1ef6d82aa65c2c2fb25c5
Liz Baldelomar <hello@lizethb.com>
2023-08-24 23:32:40 +0800

    Do Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+  if (typeof x != 'number' || typeof y != 'number' || x < 0 || y < 0) return 'ERROR';
+  let num1 = (x > y) ? y : x;
+  let num2 = (x > y) ? x : y;
+  
+  let result = 0;
+  for (let i = num1; i <= num2; i++) result += i;
+  return result;
 };
 
 // Do not edit below this line
